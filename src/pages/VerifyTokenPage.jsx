39338784e8a989d7b88b250341ed9460e9aa99c9
MuhammadHelga <128779298+MuhammadHelga@users.noreply.github.com>
2026-02-27 22:27:09 +0700

    Update VerifyTokenPage

--- src/pages/VerifyTokenPage.jsx
+++ src/pages/VerifyTokenPage.jsx
@@ -42,7 +42,7 @@ function VerifyTokenPage() {
                 <div className="text-center mb-8">
                     <h2 className="text-2xl font-bold text-[#0D2B45]">Verifikasi Akses</h2>
                     <p className="text-gray-500 text-sm mt-2">
-                        Masukkan token yang dikirimkan ke Email/WhatsApp Anda
+                        Masukkan token yang dikirimkan ke Email Anda
                     </p>
                 </div>
 
